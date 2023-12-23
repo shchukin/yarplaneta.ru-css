b254da1180e0a170b8a965c99ac1b36cd84f908c
Anton Shchukin <a.a.shchukin@gmail.com>
2023-12-23 10:06:11 +0700

    polygon draft

--- src/scripts/_polygon.js
+++ src/scripts/_polygon.js
@@ -2,19 +2,31 @@
 
     $(window).on('load', function () {
         $('.polygon').each(function () {
-            const $this = $(this)
+            const $this = $(this);
+
             const width = $this.outerWidth();
             const height = $this.outerHeight();
+            const angleWidth = 50;
+            const angleHeight = 100;
+            const stroke = 10;
 
             $this.prepend(
                 `<svg class="polygon__background" width="${width}" height="${height}">` +
-                    `<path d="M   0   0` +
-                        `L ${width - 100}   0` +
-                        `L ${width} ${100}` +
-                        `L ${width} ${height}` +
-                        `L 0  ${height}` +
-                        `Z"` +
-                        ` stroke="red" stroke-width="10" fill="transparent"/>` +
+                    `<defs>` +
+                        `<path id="ld" d="M ${stroke} ${stroke}` +
+                            `L ${width - angleWidth} ${stroke}` +
+                            `L ${width - stroke} ${angleHeight}` +
+                            `L ${width - stroke} ${height - stroke}` +
+                            `L 10  ${height - stroke}` +
+                            `Z"` +
+                        `>` +
+                            `<clipPath id="clip">` +
+                                `<use xlink:href="#ld"/>` +
+                            `</clipPath>` +
+                    `</defs>` +
+                    `<g>` +
+                        `<use id="hover" xlink:href="#ld" stroke="#0081C6" stroke-width="${stroke * 2}" fill="#00D2B8" clip-path="url(#clip)"/>` +
+                    `</g>` +
                 `</svg>`
             )
         });
