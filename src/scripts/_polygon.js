b97ed9bab402ffa88ed085639409298de8f44b0e
Anton Shchukin <a.a.shchukin@gmail.com>
2023-12-25 11:28:43 +0700

    stroke

--- src/scripts/_polygon.js
+++ src/scripts/_polygon.js
@@ -9,28 +9,51 @@
 
             const $this = $(this);
 
-            /* Ширина/высота всегда по размерам родителя */
+            /* 1) Ширина/высота всегда по размерам родителя. Неизменна. */
             const width = $this.outerWidth();
             const height = $this.outerHeight();
 
-            /* Значения по умолчанию -- подходит для кнопок */
-            let angleWidth = 17;
-            let angleHeight = 24;
+
+            /* 2) Параметрические значения -- для каждой фигуры свои.
+             * stroke (толщина бордюра), по-идее, надо бы извелкать из box-shadow в стилях,
+             * но парсить его сложно и ненадёжно. Теоретически там может быть несколько box-shadow.
+             */
+
+            let angleWidth = 0;
+            let angleHeight = 0;
             let stroke = 0;
 
-            /* Значения для .subnav */
-            if ( $this.hasClass('button--outline') ) {
-                stroke = 1;
-            }
-
-            if ( $this.hasClass('subnav__link') ) {
+            if ($this.hasClass('subnav__link')) {
                 angleWidth = 13;
                 angleHeight = 13;
                 stroke = 2;
             }
 
-            const radius = parseInt($this.css('border-bottom-left-radius')) - stroke / 2; /* "stroke / 2" потому что в SVG скругление считается иначе */
+            if ($this.hasClass('button--primary')) {
+                angleWidth = 17;
+                angleHeight = 24;
+                stroke = 0;
+            }
 
+            if ($this.hasClass('button--secondary')) {
+                angleWidth = 17;
+                angleHeight = 24;
+                stroke = 0;
+            }
+
+            if ($this.hasClass('button--outline')) {
+                angleWidth = 17;
+                angleHeight = 24;
+                stroke = 1;
+            }
+
+            /* 3) Оригинальный, стилевой border-radius нужен на кнопке, чтобы задать фон. Он там остаётся.
+             * SVG рисующая контур внутри напрямую зависит от этого border-radius, забираем его.
+             * Важно что скругление внутри svg обсчитывает не так как в стилях. По-этому вычитаем "stroke/2".
+             */
+            const radius = parseInt($this.css('border-bottom-left-radius')) - stroke / 2;
+
+            /* Линия, по которой будет обрезаться полигон и рисоваться бордюр */
             const path = `
                 M ${0} ${radius} 
                 C ${0} ${radius / 2}   ${radius / 2} ${0}   ${radius} ${0} 
@@ -47,8 +70,7 @@
             /* Вырезаем нужную форму: */
             $this.attr('style', `clip-path: path('${path}')`)
 
-            /* Бордюр в виде такой же формы: */
-
+            /* Опциональный контур в виде такой же формы: */
             if(stroke) {
 
                 $this.find('.polygon__border').remove(); /* Если с прошлого раза остался .polygon__border (например при перерисовки, при ресайзе) */
@@ -60,52 +82,52 @@
                 `);
             }
 
+            /* Даём стилям понять, что алгоритм отработан и новый контур отрисован (чтобы они выключили старый) */
             $this.addClass('polygon--applied');
-
-
-
-            /*
-              Черновик оригинального path со скруглёнными углами вокруг среза.
-              offset ещё не установлен. Захардкоженные числа слишком сложно обобщить:
-              <path id="ld2" d="
-                M ${0} ${radius}
-                C ${0} ${radius/2} ${radius/2} ${0} ${radius} ${0}
-                L ${width - angleWidth - radius/2} ${0}
-                C${width - angleWidth} ${0}
-
-                  566 8.5
-                  575 22.61
-                L 631.75 109.076705
-                C 637.130039 117.264245
-
-                ${width} ${angleHeight + radius / 2} ${width} ${angleHeight + radius}
-                L ${width} ${height - radius}
-                C ${width} ${height - radius/2} ${width - radius/2} ${height} ${width - radius} ${height}
-                L ${radius} ${height}
-                C ${radius / 2} ${height} ${0} ${height - radius/2} ${0} ${height - radius}
-                L ${0} ${radius}
-                Z
-            ">
-            */
-
-            /* Черновик варианта, когда за счёт параметра offset достигался stroke по типу inner:
-            function generatePath(offset) {
-                return `
-                    M ${0 + offset} ${radius + offset}
-                    C ${0 + offset} ${radius / 2 + offset}   ${radius / 2 + offset} ${0 + offset}   ${radius + offset} ${0 + offset}
-                    L ${width - angleWidth - offset} ${0 + offset}
-                    L ${width - offset} ${angleHeight + offset}
-                    L ${width - offset} ${height - radius - offset}
-                    C ${width - offset} ${height - radius / 2 - offset}   ${width - radius / 2 - offset} ${height - offset}   ${width - radius - offset} ${height - offset}
-                    L ${radius + offset} ${height - offset}
-                    C ${radius / 2 + offset} ${height - offset}    ${0 + offset} ${height - radius / 2 - offset}    ${0 + offset} ${height - radius - offset}
-                    L ${0 + offset} ${radius + offset}
-                    Z
-                `.replace(/\s\s+/g, ' ');
-            }
-            */
 
         });
     }
 
 })(jQuery);
+
+
+/*
+  Черновик оригинального path со скруглёнными углами вокруг среза.
+  offset ещё не установлен. Захардкоженные числа слишком сложно обобщить:
+  <path id="ld2" d="
+    M ${0} ${radius}
+    C ${0} ${radius/2} ${radius/2} ${0} ${radius} ${0}
+    L ${width - angleWidth - radius/2} ${0}
+    C${width - angleWidth} ${0}
+
+      566 8.5
+      575 22.61
+    L 631.75 109.076705
+    C 637.130039 117.264245
+
+    ${width} ${angleHeight + radius / 2} ${width} ${angleHeight + radius}
+    L ${width} ${height - radius}
+    C ${width} ${height - radius/2} ${width - radius/2} ${height} ${width - radius} ${height}
+    L ${radius} ${height}
+    C ${radius / 2} ${height} ${0} ${height - radius/2} ${0} ${height - radius}
+    L ${0} ${radius}
+    Z
+">
+*/
+
+/* Черновик варианта, когда за счёт параметра offset достигался stroke по типу inner:
+function generatePath(offset) {
+    return `
+        M ${0 + offset} ${radius + offset}
+        C ${0 + offset} ${radius / 2 + offset}   ${radius / 2 + offset} ${0 + offset}   ${radius + offset} ${0 + offset}
+        L ${width - angleWidth - offset} ${0 + offset}
+        L ${width - offset} ${angleHeight + offset}
+        L ${width - offset} ${height - radius - offset}
+        C ${width - offset} ${height - radius / 2 - offset}   ${width - radius / 2 - offset} ${height - offset}   ${width - radius - offset} ${height - offset}
+        L ${radius + offset} ${height - offset}
+        C ${radius / 2 + offset} ${height - offset}    ${0 + offset} ${height - radius / 2 - offset}    ${0 + offset} ${height - radius - offset}
+        L ${0 + offset} ${radius + offset}
+        Z
+    `.replace(/\s\s+/g, ' ');
+}
+*/
