87915185a6686b22b1acaacad792055ee0babf34
Anton Shchukin <a.a.shchukin@gmail.com>
2024-01-18 18:00:06 +0700

    clean up olygon

--- src/scripts/_polygon.js
+++ src/scripts/_polygon.js
@@ -101,33 +101,33 @@
 
             if ($this.hasClass('polygon--trapezoid')) {
                 path = `
-                    M ${0} ${radius} 
-                    C ${0} ${radius / 2}   ${radius / 2} ${0}   ${radius} ${0} 
-                    L ${width - angleWidth} ${0} 
+                    M ${0} ${radius}
+                    C ${0} ${radius / 2}   ${radius / 2} ${0}   ${radius} ${0}
+                    L ${width - angleWidth} ${0}
                     L ${width} ${angleHeight}
                     L ${width} ${height - radius}
                     C ${width} ${height - radius / 2}   ${width - radius / 2} ${height}   ${width - radius} ${height}
                     L ${radius} ${height}
                     C ${radius / 2} ${height}    ${0} ${height - radius / 2}    ${0} ${height - radius}
-                    L ${0} ${radius} 
+                    L ${0} ${radius}
                     Z
                 `;
             }
 
             if ($this.hasClass('polygon--card')) {
                 path = `
-                    M ${radius} ${0} 
-                    L ${width - radius} ${0} 
-                    C ${width - radius / 2} ${0}   ${width} ${radius / 2}     ${width} ${radius}  
-                    L ${width} ${height - angleHeight - radius} 
-                    C ${width} ${height - angleHeight - radius / 2}    ${width - radius / 2} ${height - angleHeight}    ${width - radius} ${height - angleHeight} 
-                    L ${width - angleWidth + radius} ${height - angleHeight} 
-                    C ${width - angleWidth + radius / 2} ${height - angleHeight}    ${width - angleWidth} ${height - angleHeight + radius / 2}    ${width - angleWidth} ${height - angleHeight + radius} 
-                    L ${width - angleWidth} ${height - radius} 
-                    C ${width - angleWidth} ${height - radius / 2}   ${width - angleWidth - radius / 2} ${height}   ${width - angleWidth - radius} ${height} 
-                    L ${radius} ${height} 
-                    C ${radius / 2}  ${height}     ${0} ${height - radius / 2}     ${0} ${height - radius} 
-                    L ${0} ${radius} 
+                    M ${radius} ${0}
+                    L ${width - radius} ${0}
+                    C ${width - radius / 2} ${0}   ${width} ${radius / 2}     ${width} ${radius}
+                    L ${width} ${height - angleHeight - radius}
+                    C ${width} ${height - angleHeight - radius / 2}    ${width - radius / 2} ${height - angleHeight}    ${width - radius} ${height - angleHeight}
+                    L ${width - angleWidth + radius} ${height - angleHeight}
+                    C ${width - angleWidth + radius / 2} ${height - angleHeight}    ${width - angleWidth} ${height - angleHeight + radius / 2}    ${width - angleWidth} ${height - angleHeight + radius}
+                    L ${width - angleWidth} ${height - radius}
+                    C ${width - angleWidth} ${height - radius / 2}   ${width - angleWidth - radius / 2} ${height}   ${width - angleWidth - radius} ${height}
+                    L ${radius} ${height}
+                    C ${radius / 2}  ${height}     ${0} ${height - radius / 2}     ${0} ${height - radius}
+                    L ${0} ${radius}
                     C ${0} ${radius / 2}    ${radius / 2} ${0}    ${radius} ${0}
                     Z
                 `;
