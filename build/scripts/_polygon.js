927b4d0a111baa9b5d01c12e518909c3d6d772be
Anton Shchukin <a.a.shchukin@gmail.com>
2024-02-02 13:23:14 +0700

    build

--- build/scripts/_polygon.js
+++ build/scripts/_polygon.js
@@ -36,32 +36,32 @@
 
 
             if ($this.hasClass('session__info')) {
-                angleWidth = (userScreen === 'smartphone') ? 36 : (userScreen === 'monitor') ? 52 : 45; /* Читать справа-налево, чтобы было laptop first (как в стилях) */
-                angleHeight = (userScreen === 'smartphone') ? 30 : (userScreen === 'monitor') ? 42 : 36; /* Читать справа-налево, чтобы было laptop first (как в стилях) */
+                angleWidth = (userScreen === 'smartphone') ? 38 : (userScreen === 'monitor') ? 54 : 47; /* Читать справа-налево, чтобы было laptop first (как в стилях) */
+                angleHeight = (userScreen === 'smartphone') ? 32 : (userScreen === 'monitor') ? 44 : 38; /* Читать справа-налево, чтобы было laptop first (как в стилях) */
                 stroke = 2;
             }
 
             if ($this.hasClass('docs__link')) {
-                angleWidth = (userScreen === 'smartphone') ? 20 : (userScreen === 'monitor') ? 52 : 45; /* Читать справа-налево, чтобы было laptop first (как в стилях) */
-                angleHeight = (userScreen === 'smartphone') ? 32 : (userScreen === 'monitor') ? 42 : 36; /* Читать справа-налево, чтобы было laptop first (как в стилях) */
+                angleWidth = (userScreen === 'smartphone') ? 22 : (userScreen === 'monitor') ? 54 : 47; /* Читать справа-налево, чтобы было laptop first (как в стилях) */
+                angleHeight = (userScreen === 'smartphone') ? 34 : (userScreen === 'monitor') ? 44 : 38; /* Читать справа-налево, чтобы было laptop first (как в стилях) */
                 stroke = 2;
             }
 
             if ($this.hasClass('pagination__item')) {
-                angleWidth = (userScreen === 'smartphone') ? 6 : (userScreen === 'monitor') ? 8 : 7; /* Читать справа-налево, чтобы было laptop first (как в стилях) */
-                angleHeight = (userScreen === 'smartphone') ? 9 : (userScreen === 'monitor') ? 11 : 10; /* Читать справа-налево, чтобы было laptop first (как в стилях) */
+                angleWidth = (userScreen === 'smartphone') ? 6 : (userScreen === 'monitor') ? 10 : 9; /* Читать справа-налево, чтобы было laptop first (как в стилях) */
+                angleHeight = (userScreen === 'smartphone') ? 11 : (userScreen === 'monitor') ? 13 : 12; /* Читать справа-налево, чтобы было laptop first (как в стилях) */
                 stroke = 1;
             }
 
             if ($this.hasClass('sub-nav__link')) {
-                angleWidth = (userScreen === 'smartphone') ? 11 : (userScreen === 'monitor') ? 13 : 12; /* Читать справа-налево, чтобы было laptop first (как в стилях) */
-                angleHeight = (userScreen === 'smartphone') ? 11 : (userScreen === 'monitor') ? 13 : 12; /* Читать справа-налево, чтобы было laptop first (как в стилях) */
+                angleWidth = (userScreen === 'smartphone') ? 15 : (userScreen === 'monitor') ? 17 : 16; /* Читать справа-налево, чтобы было laptop first (как в стилях) */
+                angleHeight = (userScreen === 'smartphone') ? 15 : (userScreen === 'monitor') ? 17 : 16; /* Читать справа-налево, чтобы было laptop first (как в стилях) */
                 stroke = 2;
             }
 
             if ($this.hasClass('button')) {
-                angleWidth = (userScreen === 'smartphone') ? 11 : (userScreen === 'monitor') ? 17 : 14; /* Читать справа-налево, чтобы было laptop first (как в стилях) */
-                angleHeight = (userScreen === 'smartphone') ? 18 : (userScreen === 'monitor') ? 24 : 21; /* Читать справа-налево, чтобы было laptop first (как в стилях) */
+                angleWidth = (userScreen === 'smartphone') ? 15 : (userScreen === 'monitor') ? 21 : 18; /* Читать справа-налево, чтобы было laptop first (как в стилях) */
+                angleHeight = (userScreen === 'smartphone') ? 20 : (userScreen === 'monitor') ? 26 : 23; /* Читать справа-налево, чтобы было laptop first (как в стилях) */
                 stroke = 0;
             }
 
@@ -70,14 +70,14 @@
             }
 
             if ($this.hasClass('slideshow__control')) {
-                angleWidth = (userScreen === 'smartphone') ? 7 : (userScreen === 'monitor') ? 8 : 7; /* Читать справа-налево, чтобы было laptop first (как в стилях) */
-                angleHeight = (userScreen === 'smartphone') ? 12 : (userScreen === 'monitor') ? 13 : 12; /* Читать справа-налево, чтобы было laptop first (как в стилях) */
+                angleWidth = (userScreen === 'smartphone') ? 9 : (userScreen === 'monitor') ? 10 : 9; /* Читать справа-налево, чтобы было laptop first (как в стилях) */
+                angleHeight = (userScreen === 'smartphone') ? 14 : (userScreen === 'monitor') ? 15 : 14; /* Читать справа-налево, чтобы было laptop first (как в стилях) */
                 stroke = 1;
             }
 
             if ($this.hasClass('day')) {
-                angleWidth = (userScreen === 'smartphone') ? 13 : (userScreen === 'monitor') ? 15 : 13; /* Читать справа-налево, чтобы было laptop first (как в стилях) */
-                angleHeight = (userScreen === 'smartphone') ? 10 : (userScreen === 'monitor') ? 12 : 10; /* Читать справа-налево, чтобы было laptop first (как в стилях) */
+                angleWidth = (userScreen === 'smartphone') ? 15 : (userScreen === 'monitor') ? 17 : 15; /* Читать справа-налево, чтобы было laptop first (как в стилях) */
+                angleHeight = (userScreen === 'smartphone') ? 12 : (userScreen === 'monitor') ? 14 : 12; /* Читать справа-налево, чтобы было laptop first (как в стилях) */
                 stroke = 1;
             }
 
@@ -85,8 +85,8 @@
             if ($this.hasClass('panel__body')) {
 
                 /* Отступы между карточкой и кнопкой: */
-                const topPadding = (userScreen === 'smartphone') ? 8 : (userScreen === 'monitor') ? 10 : 9; /* Читать справа-налево, чтобы было laptop first (как в стилях) */;
-                const leftPadding = (userScreen === 'smartphone') ? 6 : (userScreen === 'monitor') ? 8 : 7; /* Читать справа-налево, чтобы было laptop first (как в стилях) */;
+                const topPadding = (userScreen === 'smartphone') ? 10 : (userScreen === 'monitor') ? 12 : 11; /* Читать справа-налево, чтобы было laptop first (как в стилях) */;
+                const leftPadding = (userScreen === 'smartphone') ? 8 : (userScreen === 'monitor') ? 10 : 9; /* Читать справа-налево, чтобы было laptop first (как в стилях) */;
 
                 /* Размеры кнопки + отступы: */
                 angleWidth = parseInt($this.parents('.panel').find('.panel__action').outerWidth(), 10) + topPadding;
@@ -99,12 +99,43 @@
 
             let path = '';
 
+            // M 0,7.5
+            // C0 3.35787 3.35786,0 7.5,0
+            // L115.43,0
+            // C117.955,0 120.311,1.27115 121.698,3.38199
+            // L132.768,20.2331
+            // C133.572,21.4562 134,22.8876 134,24.3511
+            // L134,46.5
+            // C134,50.6421 130.642,54 126.5,54
+            // L7.5,54
+            // C3.35786,54 0,50.6421 0,46.5
+            // L0,39.375
+            // L0,7.5
+            // Z
+
+
+            /*
+              В коде ниже, две строчки помеченные "→" -- слабое место. Здесь скругление углов как бы захардкожено цифрами 1,2,3,6::
+              M ${0} ${radius}
+              C ${0} ${radius / 2}   ${radius / 2} ${0}   ${radius} ${0}
+              L ${width - angleWidth} ${0}
+           →  C ${width - angleWidth + 3}  ${0}   ${width - angleWidth + 5}  ${1} ${width - angleWidth + 6} ${2}
+              L ${width - 1.25}  ${angleHeight - 5}
+           →  C ${width - 0.5}  ${angleHeight - 3}      ${width}  ${angleHeight - 1.5}      ${width}   ${angleHeight}
+              L ${width} ${height - radius}
+              C ${width} ${height - radius / 2}   ${width - radius / 2} ${height}   ${width - radius} ${height}
+              L ${radius} ${height}
+              C ${radius / 2} ${height}    ${0} ${height - radius / 2}    ${0} ${height - radius}
+              L ${0} ${radius}
+            */
             if ($this.hasClass('polygon--trapezoid')) {
                 path = `
                     M ${0} ${radius}
                     C ${0} ${radius / 2}   ${radius / 2} ${0}   ${radius} ${0}
                     L ${width - angleWidth} ${0}
-                    L ${width} ${angleHeight}
+                    C ${width - angleWidth + 3}  ${0}   ${width - angleWidth + 5}  ${1} ${width - angleWidth + 6} ${2}
+                    L ${width - 1.25}  ${angleHeight - 5}
+                    C ${width - 0.5}  ${angleHeight - 3}      ${width}  ${angleHeight - 1.5}      ${width}   ${angleHeight}
                     L ${width} ${height - radius}
                     C ${width} ${height - radius / 2}   ${width - radius / 2} ${height}   ${width - radius} ${height}
                     L ${radius} ${height}
