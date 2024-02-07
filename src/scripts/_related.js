ec3e7ab11f376dc747e77b9d4832fff37162553a
Anton Shchukin <a.a.shchukin@gmail.com>
2024-02-07 16:44:36 +0700

    related

--- src/scripts/_related.js
+++ src/scripts/_related.js
@@ -42,45 +42,4 @@
         setTimeout(initRelatedCarousel, 1000)
     });
 
-
-
-    //
-    // let relatedCarousel;
-    //
-    // function initRelatedCarousel() {
-    //
-    //     if(relatedCarousel) { /* Если это не первая инициализация */
-    //         relatedCarousel.destroy();
-    //     }
-    //
-    //     if( window.matchMedia('(min-width: 740px)').matches ) { /* Только на десктопах */
-    //         relatedCarousel = new Swiper(".related__carousel--all-screens .swiper", {
-    //             navigation: {
-    //                 prevEl: '.related__control--prev',
-    //                 nextEl: '.related__control--next',
-    //             },
-    //             mousewheel: {
-    //                 forceToAxis: true,
-    //             },
-    //             slidesPerView: 2,
-    //             slidesPerGroup: 2,
-    //             spaceBetween: 16,
-    //             breakpoints: {
-    //                 1500: {
-    //                     slidesPerView: 3,
-    //                     slidesPerGroup: 3,
-    //                     spaceBetween: 24,
-    //                 },
-    //             }
-    //         });
-    //     }
-    // }
-    //
-    //
-    // $(document).ready(initRelatedCarousel);
-    //
-    // $(window).on('resize', function () {
-    //     setTimeout(initRelatedCarousel, 1000)
-    // });
-
 })(jQuery);
