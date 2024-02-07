de667c7a05b4e7cc464d8beaa57c6be9e4aa6546
Anton Shchukin <a.a.shchukin@gmail.com>
2024-02-07 16:05:15 +0700

    clean up related

--- src/scripts/_related.js
+++ src/scripts/_related.js
@@ -2,15 +2,15 @@
 
     /* Инициализируем Swiper */
 
-    let scheduleDayList;
+    let relatedCarousel;
 
-    function initScheduleDayList() {
+    function initRelatedCarousel() {
 
-        if(scheduleDayList) { /* Если это не первая инициализация */
-            scheduleDayList.destroy();
+        if(relatedCarousel) { /* Если это не первая инициализация */
+            relatedCarousel.destroy();
         }
 
-        scheduleDayList = new Swiper(".related__carousel--all-screens .swiper", {
+        relatedCarousel = new Swiper(".related__carousel .swiper", {
             navigation: {
                 prevEl: '.swiper__control--prev',
                 nextEl: '.swiper__control--next',
@@ -18,15 +18,15 @@
             mousewheel: {
                 forceToAxis: true,
             },
+            pagination: {
+                el: '.swiper__pagination',
+                clickable: true,
+            },
             slidesPerView: 1,
             slidesPerGroup: 1,
             spaceBetween: 16,
             breakpoints: {
                 740: {
-                    pagination: {
-                        el: '.swiper__pagination',
-                        clickable: true,
-                    },
                     slidesPerView: 3,
                     slidesPerGroup: 3,
                     spaceBetween: 24,
@@ -36,25 +36,25 @@
     }
 
 
-    $(document).ready(initScheduleDayList);
+    $(document).ready(initRelatedCarousel);
 
     $(window).on('resize', function () {
-        setTimeout(initScheduleDayList, 1000)
+        setTimeout(initRelatedCarousel, 1000)
     });
 
 
 
     //
-    // let scheduleDayList;
+    // let relatedCarousel;
     //
-    // function initScheduleDayList() {
+    // function initRelatedCarousel() {
     //
-    //     if(scheduleDayList) { /* Если это не первая инициализация */
-    //         scheduleDayList.destroy();
+    //     if(relatedCarousel) { /* Если это не первая инициализация */
+    //         relatedCarousel.destroy();
     //     }
     //
     //     if( window.matchMedia('(min-width: 740px)').matches ) { /* Только на десктопах */
-    //         scheduleDayList = new Swiper(".related__carousel--all-screens .swiper", {
+    //         relatedCarousel = new Swiper(".related__carousel--all-screens .swiper", {
     //             navigation: {
     //                 prevEl: '.related__control--prev',
     //                 nextEl: '.related__control--next',
@@ -77,10 +77,10 @@
     // }
     //
     //
-    // $(document).ready(initScheduleDayList);
+    // $(document).ready(initRelatedCarousel);
     //
     // $(window).on('resize', function () {
-    //     setTimeout(initScheduleDayList, 1000)
+    //     setTimeout(initRelatedCarousel, 1000)
     // });
 
 })(jQuery);
