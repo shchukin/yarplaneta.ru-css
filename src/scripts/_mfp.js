0e3a76309c326de168a5f6fcb4a66f41e5d18b30
Anton Shchukin <a.a.shchukin@gmail.com>
2024-02-09 16:05:19 +0700

    scrollbar width

--- src/scripts/_mfp.js
+++ src/scripts/_mfp.js
@@ -1,9 +1,21 @@
 (function($) {
 
+    const $fixedHeader = $('.header__desktop-panel, .header__mobile-panel');
+    const scrollWidth = $(window).outerWidth() - $(window).width();
+
+
     $('.mfp-handler').magnificPopup({
         type: 'inline',
         removalDelay: 200,
-        showCloseBtn: false
+        showCloseBtn: false,
+        callbacks: {
+            open: function() {
+                $fixedHeader.css({'margin-right': scrollWidth});
+            },
+            close: function() {
+                $fixedHeader.css({'margin-right': '0'});
+            }
+        }
     });
 
 })(jQuery);
